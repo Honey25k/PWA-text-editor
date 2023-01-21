63432185138ad378b59f79332dc70c4f79589aca
honeytech <honey93k@gmail.com>
2023-01-20 19:41:30 -0800

    added code for app

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,50 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('text', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('text')) {
+        console.log('text database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('text', { keyPath: 'id', autoIncrement: true });
+      console.log('text database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// dummy comment for heroku deploy
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+
+  const editorDb = await openDB('text', 1);
+
+  const tx = editorDb.transaction('text', 'readwrite');
+
+  const store = tx.objectStore('text');
+
+  const request = store.put({ id: 1, content: content });
+
+  const result = await request;
+
+console.log('data put request to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log ('GET from the database');
+
+  const editorDb = await openDB('text', 1);
+
+  const tx = editorDb.transaction('text', 'readonly');
+
+  const store = tx.objectStore('text');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
